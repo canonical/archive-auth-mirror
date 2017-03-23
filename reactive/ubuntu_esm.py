275d796b7815de7c739a8197772306994c14c98c
Alberto Donato <alberto.donato@gmail.com>
2017-03-23 13:41:26 +0100

    Add tests.

--- reactive/ubuntu_esm.py
+++ reactive/ubuntu_esm.py
@@ -4,6 +4,7 @@ import shutil
 from charms.reactive import when_not, set_state
 
 BASE_DIR = '/srv/ubuntu-esm'
+REPREPRO_DIR = BASE_DIR + '/repo'
 STATIC_DIR = BASE_DIR + '/static'
 
 
@@ -15,6 +16,12 @@ def install_ubuntu_esm():
 
 def _ensure_tree():
     '''Ensure the tree structure on the filesystem is set up.'''
-    if not os.path.isdir(STATIC_DIR):
-        os.makedirs(STATIC_DIR)
+    _ensure_dir(STATIC_DIR)
+    _ensure_dir(REPREPRO_DIR)
     shutil.copy('resources/index.html', STATIC_DIR)
+
+
+def _ensure_dir(path):
+    '''Ensure the directory exists.'''
+    if not os.path.isdir(path):
+        os.makedirs(path)
