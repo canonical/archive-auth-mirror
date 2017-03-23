afcb23a2618598bcdcea96fe8ad823733571999f
Alberto Donato <albertodonato@users.noreply.github.com>
2017-03-23 18:07:20 +0000

    Install hook (#1)
    Add basic logic to install hook.

--- reactive/ubuntu_esm.py
+++ reactive/ubuntu_esm.py
@@ -1,6 +1,21 @@
+import shutil
+from pathlib import Path
+
 from charms.reactive import when_not, set_state
+
+BASE_DIR = Path('/srv/ubuntu-esm')
+REPREPRO_DIR = BASE_DIR / 'repo'
+STATIC_DIR = BASE_DIR / 'static'
 
 
 @when_not('ubuntu-esm.installed')
-def install_ubuntu_esm():
+def install():
+    _install_resources()
     set_state('ubuntu-esm.installed')
+
+
+def _install_resources():
+    '''Create tree structure and copy resources from the charm.'''
+    for directory in BASE_DIR, STATIC_DIR, REPREPRO_DIR:
+        directory.mkdir(exist_ok=True)
+    shutil.copy('resources/index.html', str(STATIC_DIR))
