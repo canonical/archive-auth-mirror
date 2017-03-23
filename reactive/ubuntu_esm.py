ecc00a3497293da754c4e21a7b8eeae84163bc46
Alberto Donato <alberto.donato@gmail.com>
2017-03-23 17:04:34 +0100

    Use pathlib.

--- reactive/ubuntu_esm.py
+++ reactive/ubuntu_esm.py
@@ -1,27 +1,21 @@
-import os
 import shutil
+from pathlib import Path
 
 from charms.reactive import when_not, set_state
 
-BASE_DIR = '/srv/ubuntu-esm'
-REPREPRO_DIR = BASE_DIR + '/repo'
-STATIC_DIR = BASE_DIR + '/static'
+BASE_DIR = Path('/srv/ubuntu-esm')
+REPREPRO_DIR = BASE_DIR / 'repo'
+STATIC_DIR = BASE_DIR / 'static'
 
 
 @when_not('ubuntu-esm.installed')
 def install():
-    _ensure_tree()
+    _install_resources()
     set_state('ubuntu-esm.installed')
 
 
-def _ensure_tree():
-    '''Ensure the tree structure on the filesystem is set up.'''
-    _ensure_dir(STATIC_DIR)
-    _ensure_dir(REPREPRO_DIR)
-    shutil.copy('resources/index.html', STATIC_DIR)
-
-
-def _ensure_dir(path):
-    '''Ensure the directory exists.'''
-    if not os.path.isdir(path):
-        os.makedirs(path)
+def _install_resources():
+    '''Create tree structure and copy resources from the charm.'''
+    for directory in BASE_DIR, STATIC_DIR, REPREPRO_DIR:
+        directory.mkdir(exist_ok=True)
+    shutil.copy('resources/index.html', str(STATIC_DIR))
