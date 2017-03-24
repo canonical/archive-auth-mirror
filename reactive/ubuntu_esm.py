a137b7485f9557261c11e9d7b25db2e3e3e88d4f
Alberto Donato <alberto.donato@gmail.com>
2017-03-24 10:00:32 +0100

    Add mirroring script.

--- reactive/ubuntu_esm.py
+++ reactive/ubuntu_esm.py
@@ -3,8 +3,18 @@ from pathlib import Path
 
 from charms.reactive import when_not, set_state
 
+# The filesystem tree for the service is as follows:
+#
+#  /srv/ubuntu-esm
+#  ├── bin
+#  │   └── ubuntu-esm-mirror  -- the mirroring script
+#  ├── reprepro
+#  │   └── conf -- reprepro configuration files
+#  └── static   -- the root of the virtualhost, contains the repository
+#
 BASE_DIR = Path('/srv/ubuntu-esm')
-REPREPRO_DIR = BASE_DIR / 'repo'
+REPREPRO_CONF_DIR = BASE_DIR / 'reprepro/conf'
+BIN_DIR = BASE_DIR / 'bin'
 STATIC_DIR = BASE_DIR / 'static'
 
 
@@ -16,6 +26,7 @@ def install():
 
 def _install_resources():
     '''Create tree structure and copy resources from the charm.'''
-    for directory in BASE_DIR, STATIC_DIR, REPREPRO_DIR:
+    for directory in BASE_DIR, BIN_DIR, REPREPRO_CONF_DIR, STATIC_DIR:
         directory.mkdir(exist_ok=True)
     shutil.copy('resources/index.html', str(STATIC_DIR))
+    shutil.copy('resources/ubuntu-esm-mirror', str(BIN_DIR))
