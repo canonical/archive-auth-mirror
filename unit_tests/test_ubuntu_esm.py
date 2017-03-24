0d7eb74b45d83bc2c9f2467a73038cdfff6d1f9c
Alberto Donato <alberto.donato@gmail.com>
2017-03-24 10:32:09 +0100

    Base unittest setup.

--- unit_tests/test_ubuntu_esm.py
+++ unit_tests/test_ubuntu_esm.py
@@ -1,7 +1,10 @@
 from charmtest import CharmTest
 
+from reactive import ubuntu_esm
+
 
 class SampleTest(CharmTest):
 
     def test_foo(self):
+        print(ubuntu_esm)
         self.assertTrue(True)
