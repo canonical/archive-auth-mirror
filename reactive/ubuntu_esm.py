3466fe53968a5a766a7b9ba4b7e0f111b05f629f
Alberto Donato <albertodonato@users.noreply.github.com>
2017-03-24 10:05:40 +0000

    Website relation (#2)
    Add hook for configuring website relation.

--- reactive/ubuntu_esm.py
+++ reactive/ubuntu_esm.py
@@ -1,7 +1,14 @@
 import shutil
 from pathlib import Path
+import textwrap
 
-from charms.reactive import when_not, set_state
+from charms.reactive import (
+    when_not,
+    set_state,
+)
+from charms.reactive.decorators import hook
+from charmhelpers.core import hookenv
+
 
 BASE_DIR = Path('/srv/ubuntu-esm')
 REPREPRO_DIR = BASE_DIR / 'repo'
@@ -14,8 +21,38 @@ def install():
     set_state('ubuntu-esm.installed')
 
 
+@hook('static-website-relation-{joined,changed}')
+def website_relation():
+    config = _get_website_relation_config(domain=hookenv.unit_public_ip())
+    hookenv.relation_set(hookenv.relation_id(), config)
+
+
 def _install_resources():
     '''Create tree structure and copy resources from the charm.'''
     for directory in BASE_DIR, STATIC_DIR, REPREPRO_DIR:
         directory.mkdir(exist_ok=True)
     shutil.copy('resources/index.html', str(STATIC_DIR))
+
+
+def _get_website_relation_config(domain=None):
+    '''Return the configuration for the 'static-website' relation.'''
+    vhost_config = textwrap.dedent(
+        '''
+        <VirtualHost {domain}:80>
+          DocumentRoot "{document_root}"
+          Options +Indexes
+
+          <Location />
+            Require all granted
+            Options +Indexes
+          </Location>
+        </VirtualHost>
+        '''.format(
+            domain=domain,
+            document_root=STATIC_DIR))
+    return {
+        'domain': domain,
+        'enabled': True,
+        'site_config': vhost_config,
+        'site_modules': ['autoindex'],
+        'ports': '80'}
