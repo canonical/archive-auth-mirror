78b5f8fa56585be73191c955de7ae8cf8a6e1886
Alberto Donato <alberto.donato@gmail.com>
2017-03-24 10:50:46 +0100

    WIP.

--- unit_tests/test_ubuntu_esm.py
+++ unit_tests/test_ubuntu_esm.py
@@ -1,10 +1,15 @@
+import os
+
 from charmtest import CharmTest
 
 from reactive import ubuntu_esm
 
 
-class SampleTest(CharmTest):
+class InstallResourcesTests(CharmTest):
 
-    def test_foo(self):
-        print(ubuntu_esm)
-        self.assertTrue(True)
+    def test_dirs(self):
+        '''The _install_resources function creates the filesystem structure.'''
+        root_path = self.fakes.fs.root.path
+        self.fakes.fs.add('/srv')
+        ubuntu_esm._install_resources()
+        print(os.listdir(root_path))
