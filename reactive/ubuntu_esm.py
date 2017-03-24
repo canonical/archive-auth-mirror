d8d5a5cec3ad541fead56429529a78a9d539b2c1
Alberto Donato <albertodonato@users.noreply.github.com>
2017-03-24 18:00:50 +0000

    Add unittests (#4)
    Add unittests.

--- reactive/ubuntu_esm.py
+++ reactive/ubuntu_esm.py
@@ -10,21 +10,6 @@ from charms.reactive.decorators import hook
 from charmhelpers.core import hookenv
 
 
-# The filesystem tree for the service is as follows:
-#
-#  /srv/ubuntu-esm
-#  ├── bin
-#  │   └── ubuntu-esm-mirror  -- the mirroring script
-#  ├── reprepro
-#  │   └── conf -- reprepro configuration files
-#  └── static   -- the root of the virtualhost, contains the repository
-#
-BASE_DIR = Path('/srv/ubuntu-esm')
-REPREPRO_CONF_DIR = BASE_DIR / 'reprepro/conf'
-BIN_DIR = BASE_DIR / 'bin'
-STATIC_DIR = BASE_DIR / 'static'
-
-
 @when_not('ubuntu-esm.installed')
 def install():
     _install_resources()
@@ -33,20 +18,44 @@ def install():
 
 @hook('static-website-relation-{joined,changed}')
 def website_relation():
-    config = _get_website_relation_config(domain=hookenv.unit_public_ip())
+    config = _get_website_relation_config(hookenv.unit_public_ip())
     hookenv.relation_set(hookenv.relation_id(), config)
 
 
-def _install_resources():
+def _get_paths(base_dir=None):
+    '''Return path for the service tree.
+
+    The filesystem tree for the service is as follows:
+
+    /srv/ubuntu-esm
+    ├── bin
+    │   └── ubuntu-esm-mirror  -- the mirroring script
+    ├── reprepro
+    │   └── conf  -- reprepro configuration files
+    └── static  -- the root of the virtualhost, contains the repository
+    '''
+    if base_dir is None:
+        base_dir = '/srv/ubuntu-esm'
+    base_dir = Path(base_dir)
+    return {
+        'base': base_dir,
+        'bin': base_dir / 'bin',
+        'reprepro': base_dir / 'reprepro',
+        'static': base_dir / 'static'}
+
+
+def _install_resources(base_dir=None):
     '''Create tree structure and copy resources from the charm.'''
-    for directory in BASE_DIR, BIN_DIR, REPREPRO_CONF_DIR, STATIC_DIR:
+    paths = _get_paths(base_dir=base_dir)
+    for directory in paths.values():
         directory.mkdir(parents=True, exist_ok=True)
-    shutil.copy('resources/index.html', str(STATIC_DIR))
-    shutil.copy('resources/ubuntu-esm-mirror', str(BIN_DIR))
+    shutil.copy('resources/index.html', str(paths['static']))
+    shutil.copy('resources/ubuntu-esm-mirror', str(paths['bin']))
 
 
-def _get_website_relation_config(domain=None):
+def _get_website_relation_config(domain):
     '''Return the configuration for the 'static-website' relation.'''
+    paths = _get_paths()
     vhost_config = textwrap.dedent(
         '''
         <VirtualHost {domain}:80>
@@ -60,7 +69,7 @@ def _get_website_relation_config(domain=None):
         </VirtualHost>
         '''.format(
             domain=domain,
-            document_root=STATIC_DIR))
+            document_root=paths['static']))
     return {
         'domain': domain,
         'enabled': True,
