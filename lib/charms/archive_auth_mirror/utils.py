a1d7719ba46ae0542576ce3c5d12ff34ddbc7614
Alberto Donato <albertodonato@users.noreply.github.com>
2017-03-28 19:16:15 +0100

    Generate reprepro configuration (#13)

--- lib/charms/archive_auth_mirror/utils.py
+++ lib/charms/archive_auth_mirror/utils.py
@@ -6,18 +6,19 @@ from charmhelpers.core import hookenv
 
 
 def get_paths(base_dir=None):
-    '''Return path for the service tree.
+    """Return path for the service tree.
 
     The filesystem tree for the service is as follows:
 
     /srv/archive-auth-mirror
     ├── bin
     │   └── mirror-archive  -- the mirroring script
+    ├── config  -- the script configuration file
     ├── reprepro
     │   └── conf  -- reprepro configuration files
     │       └── .gnupg  -- GPG config for reprepro
     └── static  -- the root of the virtualhost, contains the repository
-    '''
+    """
     if base_dir is None:
         base_dir = '/srv/archive-auth-mirror'
     base_dir = Path(base_dir)
@@ -25,19 +26,21 @@ def get_paths(base_dir=None):
     return {
         'base': base_dir,
         'bin': base_dir / 'bin',
+        'config': base_dir / 'config',
         'reprepro': reprepro_dir,
+        'reprepro-conf': reprepro_dir / 'conf',
         'static': base_dir / 'static',
         'gnupghome': reprepro_dir / '.gnupg'}
 
 
 def get_virtualhost_name(hookenv=hookenv):
-    '''Return the configured service URL or the unit address.'''
+    """Return the configured service URL or the unit address."""
     service_url = hookenv.config().get('service-url')
     return service_url or hookenv.unit_public_ip()
 
 
 def configure_website_relation():
-    '''Configure the 'static-website' relation.'''
+    """Configure the 'static-website' relation."""
     domain = get_virtualhost_name()
     config = get_website_relation_config(domain)
     for relation_id in hookenv.relation_ids('static-website'):
@@ -45,7 +48,7 @@ def configure_website_relation():
 
 
 def get_website_relation_config(domain):
-    '''Return the configuration for the 'static-website' relation.'''
+    """Return the configuration for the 'static-website' relation."""
     port = 80
     paths = get_paths()
     vhost_config = textwrap.dedent(
@@ -70,8 +73,8 @@ def get_website_relation_config(domain):
 
 
 def install_resources(base_dir=None):
-    '''Create tree structure and copy resources from the charm.'''
+    """Create tree structure and copy resources from the charm."""
     paths = get_paths(base_dir=base_dir)
-    for directory in paths.values():
-        directory.mkdir(parents=True, exist_ok=True)
+    for name in ('bin', 'reprepro-conf', 'static', 'gnupghome'):
+        paths[name].mkdir(parents=True, exist_ok=True)
     shutil.copy('resources/mirror-archive', str(paths['bin']))
---
+++ unit_tests/test_reprepro.py
@@ -0,0 +1,97 @@
+import textwrap
+import unittest
+
+from charmtest import CharmTest
+
+from charms.archive_auth_mirror.utils import get_paths
+from charms.archive_auth_mirror.reprepro import (
+    configure_reprepro,
+    disable_mirroring,
+    split_repository_uri,
+)
+
+
+class ConfigureRepreproTest(CharmTest):
+
+    def test_configuration_files(self):
+        """configure_reprepro writes rerepro config files."""
+        paths = get_paths(base_dir=self.fakes.fs.root.path)
+        uri = 'https://user:pass@example.com/ubuntu xenial main universe'
+        configure_reprepro(
+            uri, 'i386 amd64', 'ABABABAB', 'CDCDCDCD', get_paths=lambda: paths)
+        self.assertEqual(
+            textwrap.dedent(
+                '''\
+                Codename: xenial
+                Label: xenial archive
+                Components: main universe
+                UDebComponents: main universe
+                Architectures: i386 amd64
+                SignWith: CDCDCDCD
+                Update: update-repo
+                '''),
+            (paths['reprepro-conf'] / 'distributions').read_text())
+        self.assertEqual(
+            textwrap.dedent(
+                '''\
+                Name: update-repo
+                Method: https://user:pass@example.com/ubuntu
+                Suite: xenial
+                Components: main universe
+                UDebComponents: main universe
+                Architectures: i386 amd64
+                VerifyRelease: ABABABAB
+                '''),
+            (paths['reprepro-conf'] / 'updates').read_text())
+        self.assertEqual(
+            'SUITE=xenial\n', paths['config'].read_text())
+
+
+class DisableMirroringTest(CharmTest):
+
+    def test_disable_mirroring(self):
+        """disable_mirroring renames the script config file."""
+        paths = get_paths(base_dir=self.fakes.fs.root.path)
+        uri = 'https://user:pass@example.com/ubuntu xenial main universe'
+        configure_reprepro(
+            uri, 'i386 amd64', 'ABABABAB', 'CDCDCDCD', get_paths=lambda: paths)
+
+        config = paths['config']
+        self.assertTrue(config.exists())
+        orig_content = config.read_text()
+        disable_mirroring(get_paths=lambda: paths)
+        self.assertFalse(config.exists())
+        # The file is moved to .disabled
+        disabled_file = config.with_suffix('.disabled')
+        self.assertTrue(disabled_file.exists())
+        self.assertEqual(orig_content, disabled_file.read_text())
+
+    def test_disable_not_enabled(self):
+        """Disabling mirror when not configured is a no-op."""
+        paths = get_paths(base_dir=self.fakes.fs.root.path)
+        config = paths['config']
+        disable_mirroring(get_paths=lambda: paths)
+        self.assertFalse(config.exists())
+        self.assertFalse(config.with_suffix('.disabled').exists())
+
+    def test_disable_twice_(self):
+        """disable_mirroring can be called multiple times."""
+        paths = get_paths(base_dir=self.fakes.fs.root.path)
+        config = paths['config']
+        disable_mirroring(get_paths=lambda: paths)
+        disable_mirroring(get_paths=lambda: paths)
+        self.assertFalse(config.exists())
+        self.assertFalse(config.with_suffix('.disabled').exists())
+
+
+class SplitRepositoryUriTest(unittest.TestCase):
+
+    def test_uri(self):
+        """split_repo_uri splits the repository URI into tokens."""
+        tokens = split_repository_uri(
+            'https://user:pass@example.com/ubuntu xenial main universe')
+        self.assertEqual(
+            {'url': 'https://user:pass@example.com/ubuntu',
+             'suite': 'xenial',
+             'components': 'main universe'},
+            tokens)
