6d306dbee7bc33317a4311d8e3146bc543dea049
Bjorn Tillenius <bjorn@canonical.com>
2017-04-11 09:20:18 +0000

    Add unit tests.

--- lib/archive_auth_mirror/ssh.py
+++ lib/archive_auth_mirror/ssh.py
@@ -3,16 +3,19 @@ import subprocess
 
 def create_key(path):
     subprocess.check_call([
-        'ssh-keygen', '-f', str(path), '-t', 'rsa', '-N', ''])
+        'ssh-keygen', '-q', '-f', str(path), '-t', 'rsa', '-N', ''])
 
 
 def add_authorized_key(public_key, authorized_keys_path):
     ssh_dir = authorized_keys_path.parent
     if not ssh_dir.exists():
         ssh_dir.mkdir(0o700)
-    with authorized_keys_path.open('r') as authorized_keys_file:
-        authorized_keys = authorized_keys_file.readlines()
+    if authorized_keys_path.exists():
+        authorized_keys = authorized_keys_path.read_text().splitlines()
+    else:
+        authorized_keys = []
+    public_key = public_key.strip()
     if public_key in authorized_keys:
         return
     with authorized_keys_path.open('a') as authorized_keys_file:
-        authorized_keys_file.write(public_key)
+        authorized_keys_file.write(public_key + '\n')
