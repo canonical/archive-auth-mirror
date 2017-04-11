7f22a1a6ad826008fecb80aa845e73009908d42a
Alberto Donato <albertodonato@users.noreply.github.com>
2017-04-11 09:30:18 +0100

    Log reprepro stdout/stderr to syslog (#32)

--- lib/archive_auth_mirror/scripts/mirror_archive.py
+++ lib/archive_auth_mirror/scripts/mirror_archive.py
@@ -1,7 +1,10 @@
 """Mirror and update a repository."""
 
+import subprocess
+import sys
+
 from ..utils import get_paths
-from ..reprepro import reprepro
+from ..reprepro import Reprepro
 from ..script import (
     setup_logger,
     get_config,
@@ -9,14 +12,20 @@ from ..script import (
 
 
 def main():
-    logger = setup_logger()
+    logger = setup_logger(echo=True)
     paths = get_paths()
     suite = get_config(paths['config'])['suite']
 
     logger.info('starting mirroring')
-    reprepro(
-        '--show-percent', '--export=never', '--keepunreferencedfiles',
-        'update', suite)
-    reprepro('export', suite)
-    reprepro('deleteunreferenced')
-    logger.info('mirroring completed')
+
+    reprepro = Reprepro(logger)
+    try:
+        reprepro.execute(
+            '--show-percent', '--export=never', '--keepunreferencedfiles',
+            'update', suite)
+        reprepro.execute('export', suite)
+        reprepro.execute('deleteunreferenced')
+        logger.info('mirroring completed')
+    except subprocess.CalledProcessError:
+        logger.error('mirroring failed')
+        sys.exit(1)
